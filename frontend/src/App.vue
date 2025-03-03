1ae81549e686cef3a1dd13a1981d85e1c7060b4d
pauldamsa <paul_damsa9@yahoo.com>
2025-03-03 20:16:46 +0200

    mobile friendly fixes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="min-h-screen bg-gray-50">
         <div class="flex h-screen overflow-hidden">
-            <Sidebar2 v-if="session.user" />
-            <div class="flex-1 flex flex-col w-0" :class="{ 'ml-14': true, 'lg:ml-60': true }">
-                <Navbar v-if="session.user" />
+            <Sidebar2 v-if="session.user" ref="sidebar" />
+            <div class="flex-1 flex flex-col w-0" :class="{ 'ml-0 lg:ml-60': true }">
+                <div class="relative">
+                    <Navbar v-if="session.user" />
+                </div>
                 <main class="flex-1 overflow-y-auto mt-16">
                     <router-view />
                 </main>
@@ -13,15 +15,23 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { titleMap } from './utils/titleMap';
 import Navbar from '@/components/Navbar.vue';
-import Sidebar from '@/components/Sidebar.vue';
 import Sidebar2 from '@/components/Sidebar2.vue';
+import { FeatherIcon } from 'frappe-ui';
 import { session } from './data/session.js';
 
 const route = useRoute();
+const sidebar = ref(null);
+
+function toggleSidebar() {
+    if (sidebar.value?.toggleMobileMenu) {
+        sidebar.value.toggleMobileMenu();
+    }
+}
 
 // Watch for route changes and update document title
 watch(
